6b9c40efb5d0c0440ba61e4acfcfdeba9f351784
hengyisu <rightguy3344@gmail.com>
2017-06-18 10:04:24 +0800

    Add files via upload

---
+++ upload_1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+
+
+<head>
+
+	<meta charset="utf-8">
+
+
+
+	<title>你要上傳幾個檔案</title>
+</head>
+<body>
+
+
+
+你要上傳幾個檔案
+	<form action="upload_2.php" method="GET" >
+		
+		<input type="text" name="howmany" >
+
+		<input type="submit" name="submit">
+
+
+	</form>
+	
+
+
+
+
+
+</body>
+</html>
